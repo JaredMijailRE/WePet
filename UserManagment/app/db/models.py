ec2fe2530d9c94cb52ed0ff7cb2ca52c2f06964c
Nicoropi <nirodriguezpi@unal.edu.co>
2025-11-11 21:40:32 -0500

    db user management

---
+++ UserManagment/app/db/models.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timezone
+import uuid
+import enum
+
+from sqlalchemy import Column, String, Date, Enum, Boolean, TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID
+from .database import Base
+
+class EmotionalStatus(enum.Enum):
+    HAPPY = "happy"
+    SAD = "sad"
+    NERVOUS = "nervous"
+    ANXIOUS = "anxious"
+
+    CALM = "calm"
+    EXITED = "exited"
+    FEARFUL = "fearful"
+    ANGRY = "angry"
+    DISGUSTED = "disgusted"
+    SURPRISED = "surprised"
+    BORED = "bored"
+    DISAPPOINTED = "disappointed"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    username = Column(String(50), nullable=False, unique=True, index=True)
+    email = Column(String(50), nullable=False, unique=True, index=True)
+    password_hash = Column(String(50), nullable=False)
+    birth_date = Column(Date, nullable=False)
+    current_emotional_status = Column(Enum(EmotionalStatus), nullable=True)
+    its_sharing_location = Column(Boolean, default=False)
+    created_at = Column(TIMESTAMP(timezone=True), default=datetime.now(timezone.utc))
+
+    # if needed, maybe use __repr__ for debuggin? something like this
+    # def __repr__(self):
+    #     return f"<User(username='{self.username}', email='{self.email}')>"
