7c7b662f60c3e12f465405b72aa6664856dfdb37
Nicoropi <nirodriguezpi@unal.edu.co>
2025-11-26 17:23:39 -0500

    almost done with the groups main page :D

--- Frontend/app/(group)/_layout.tsx
+++ Frontend/app/(group)/_layout.tsx
@@ -1,16 +1,44 @@
 import { Tabs } from 'expo-router'
+import { View } from 'react-native'
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
     return (
         <Tabs 
-        screenOptions={{ 
-            tabBarActiveTintColor: '#5c6cd1',
-            headerStyle: {backgroundColor: '#fff'},
+        screenOptions={{
+            tabBarActiveTintColor: '#9c76c2',
             headerShadowVisible: false,
+            headerStyle: {backgroundColor: '#fff'},
+            headerTitleContainerStyle: {paddingLeft: 8},
             headerTintColor: '#25292e',
-            tabBarStyle: {backgroundColor: '#25292e'}
-            }}
+
+            tabBarStyle: {
+                backgroundColor: '#f3edf7',
+                borderTopWidth: 0, 
+
+                position: 'absolute',
+                bottom: 25, 
+                marginHorizontal: 20, 
+                borderRadius: 30, 
+                height: 70, 
+
+                shadowColor: '#000',
+                shadowOffset: { width: 0, height: 10 },
+                shadowOpacity: 0.12,
+                shadowRadius: 5.84,
+                elevation: 5,
+            },
+            tabBarItemStyle: {
+                marginTop: 7,
+            },
+            tabBarLabelStyle: {
+                marginTop: 2, 
+                marginBottom: 5, 
+                fontSize: 12,
+            },
+
+            tabBarBackground: () => (<View style={{ flex: 1, backgroundColor: 'transparent' }} />),
+        }}
         >
             
             <Tabs.Screen 
--- Frontend/app/(group)/groups.tsx
+++ Frontend/app/(group)/groups.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import { View, TextInput, FlatList, Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import { View, TextInput, FlatList, Text, StyleSheet, Alert, TouchableOpacity, Platform, Image } from 'react-native';
 
 const data = [
   { group_id: '1', group_name: 'groupA' },
@@ -11,8 +11,10 @@ const data = [
 ];
 
 const GroupsSearchTab = () => {
+  const backgroundImage = require('../../assets/images/backgroundImage.jpeg');
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredData, setFilteredData] = useState(data);
+  const [isFocused, setIsFocused] = useState(false);
 
   const handleSearch = (text:string) => {
     setSearchQuery(text);
@@ -26,55 +28,124 @@ const GroupsSearchTab = () => {
     setFilteredData(newData);
   };
 
-  const handlePress = () => {
+  const createGroup = () => {
     Alert.alert('Custom Button Pressed!');
   };
 
+  interface GroupItem {
+    group_id: string; 
+    group_name: string;
+  }
+
+  const groupItem = ({ item }: { item: GroupItem }) => (
+    <TouchableOpacity 
+      style={styles.button}
+      onPress={() => console.log('Botón presionado:', item.group_name)}
+    >
+      <View style={styles.listItemContainer}>
+        <Text style={{ color: "#ffa052", fontSize: 32, paddingBottom: 3}}>•</Text>
+        <View>
+
+        </View>
+        <Text style={styles.buttonText}>{item.group_name}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+
   return (
     <View style={styles.container}>
-      <View style={styles.searchContainer}>
+      <Image 
+        source={backgroundImage} 
+        style={styles.backgroundImage}
+        // Usa 'contain' o 'center' si no quieres que se estire
+        resizeMode="contain" 
+      />
+
+      <View style={styles.searchAddContainer}>
+        <View style={[styles.searchContainer, isFocused && styles.searchContainerFocused]}>
         <TextInput
-          style={styles.searchInput}
+          style={[ styles.searchInput, Platform.OS === 'web' && { outlineStyle: 'none' as any } ]}
           placeholder="Search..."
           value={searchQuery}
           onChangeText={handleSearch}
+          onFocus={() => setIsFocused(true)} 
+          onBlur={() => setIsFocused(false)} 
+          underlineColorAndroid="transparent"
         />
-        <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <FontAwesome6 name="magnifying-glass" size={18} color="black" style={styles.searchIcon}/>
+        </View>
+
+        <TouchableOpacity style={styles.plusButton} onPress={createGroup}>
           <FontAwesome6 name="plus" size={15} color="black" />
-          <Text style={styles.buttonText}></Text>
         </TouchableOpacity>
       </View>
       
       <FlatList
         data={filteredData}
         keyExtractor={item => item.group_id}
-        renderItem={({ item }) => <Text style={styles.listItem}>{item.group_name}</Text>}
+        renderItem={ groupItem }
       />
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    position: 'absolute', 
+    // La centra respecto al contenedor padre
+    top: 0, 
+    left: 0, 
+    bottom: 0, 
+    right: 0,
+    // La opacidad la hace menos intrusiva
+    opacity: 0.3, 
+    // Ajusta el tamaño de la imagen según tus necesidades:
+    width: '100%', 
+    height: '100%',
+  },
+
   container: {
     flex: 1,
     paddingTop: 30,
-    paddingHorizontal: 20,
-    backgroundColor: '#fff'
+    paddingHorizontal: 0,
+    backgroundColor: '#fff',
+    position: 'relative',
   },
-  searchInput: {
-    height: 40,
+
+  searchAddContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+  },
+  searchContainer: {
+    flex: 9,
+    flexDirection: 'row',
+    alignItems: 'center',
     borderColor: 'gray',
     borderWidth: 1,
     borderRadius: 20,
-    paddingHorizontal: 10,
     marginTop: 5,
     marginBottom: 15,
-    flex: 9,
+    paddingHorizontal: 5,
   },
-  searchContainer: {
-    flexDirection: 'row',
+  searchContainerFocused: {
+    borderColor: '#b300ffff', 
+    borderWidth: 2,
+    marginTop: 4,
+    marginBottom: 14,
+    paddingHorizontal: 4,
   },
-  button: {
+  searchInput: {
+    height: 40,
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    flex: 1,
+  },
+  searchIcon: {
+    marginRight: 10,
+  },
+
+  plusButton: {
     backgroundColor: '#fff',
     padding: 1,
     margin: 5,
@@ -88,15 +159,19 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 4,
   },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
+
+  listItemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 20,
   },
-  listItem: {
+  button: {
     paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    paddingHorizontal: 10,
+  },
+  buttonText: {
+    color: '#000',
+    fontWeight: 'bold',
   },
 });
 
