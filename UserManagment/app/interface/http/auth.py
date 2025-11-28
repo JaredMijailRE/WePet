e35ba165872d5a5419f322903ca12c2a89eb6130
jvaquiro <jvaquiro@unal.edu.co>
2025-11-27 23:50:40 -0500

    feat(api): Add user registration endpoint

--- UserManagment/app/interface/http/auth.py
+++ UserManagment/app/interface/http/auth.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 from application.dto.auth_dto import LoginDTO
 from app.adapter.db.database import get_db
 from app.application.usecases.user_login import LoginUser
 from app.adapter.db.user_repository_sql import SQLUserRepository
-from domain.services.token_service import JWTTokenGenerator
+from app.adapter.auth.jwt_service import JWTTokenGenerator
+from app.application.dto.user_dto import UserCreate, UserResponse
+from app.application.usecases.user_register import RegisterUser
 
 
 def get_auth_router() -> APIRouter:
@@ -17,8 +20,14 @@ def get_auth_router() -> APIRouter:
         useCaseUserLogin = LoginUser(userRepository, tokenService)
         return useCaseUserLogin.execute(data.username, data.password)
 
-    @router.post("/register")
-    async def register():
-        return {"message": "Register endpoint"}
+    @router.post("/register", response_model=UserResponse)
+    async def register(user_data: UserCreate, db: Session = Depends(get_db)):
+      
+        user_repo = SQLUserRepository(db)
+        
+        use_case = RegisterUser(user_repo)
+        
+    
+        return use_case.execute(user_data)
 
     return router
