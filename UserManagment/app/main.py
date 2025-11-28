5277bf11b75c593647513a7f28ef930e936f707f
jvaquiro <jvaquiro@unal.edu.co>
2025-11-27 23:50:41 -0500

    chore(db): Ensure database schema is created on startup

--- UserManagment/app/main.py
+++ UserManagment/app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from .interface.http.auth import get_auth_router   
+from app.adapter.db.database import engine
+from app.adapter.db import models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
