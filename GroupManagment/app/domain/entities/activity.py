881ea6db07f869be4a6f60af5f5b23270ea2319c
Daniari2000 <ariadnarueda@gmail.com>
2025-11-30 10:20:32 -0500

    Agregar entidad actividad y nuevos métodos para actividad

---
+++ GroupManagment/app/domain/entities/activity.py
@@ -0,0 +1,16 @@
+import uuid
+from dataclasses import dataclass
+from datetime import datetime
+from app.adapter.db.models import ActivityStatus
+
+@dataclass
+class Activity:
+    id: uuid.UUID
+    group_id: uuid.UUID
+    title: str
+    description: str | None
+    start_date: datetime
+    end_date: datetime
+    xp_reward: int
+    status: ActivityStatus
+    created_at: datetime
--- GroupManagment/app/domain/repositories/group_repository.py
+++ GroupManagment/app/domain/repositories/group_repository.py
@@ -1,10 +1,16 @@
 from abc import ABC, abstractmethod
 from app.domain.entities.group import Group
+from app.domain.entities.group import Activity
 import uuid
+from typing import List
 
 class GroupRepository(ABC):
     @abstractmethod
     def save(self, group: Group) -> Group:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, group_id: uuid.UUID) -> Group | None:
         pass
 
     @abstractmethod
@@ -17,4 +23,48 @@ class GroupRepository(ABC):
     
     @abstractmethod
     def is_member(self, group_id: uuid.UUID, user_id: uuid.UUID) -> bool:
+        pass
+
+    @abstractmethod
+    def update_name(self, group_id: uuid.UUID, name: str) -> Group | None:
+        pass
+
+    @abstractmethod
+    def delete(self, group_id: uuid.UUID) -> bool:
+        pass
+
+    @abstractmethod
+    def get_member(self, group_id: uuid.UUID, user_id: uuid.UUID):
+        pass
+
+    @abstractmethod
+    def list_members(self, group_id: uuid.UUID):
+        pass
+
+    @abstractmethod
+    def update_member(self, group_id: uuid.UUID, user_id: uuid.UUID, role=None, is_sharing_location_with_group=None, has_notifications_enabled=None):
+        pass
+
+    @abstractmethod
+    def remove_member(self, group_id: uuid.UUID, user_id: uuid.UUID) -> bool:
+        pass
+    
+    @abstractmethod
+    def save_activity(self, activity: Activity) -> Activity:
+        pass
+
+    @abstractmethod
+    def find_activity_by_id(self, activity_id: uuid.UUID) -> Activity | None:
+        pass
+
+    @abstractmethod
+    def list_activities_by_group(self, group_id: uuid.UUID) -> List[Activity]:
+        pass
+
+    @abstractmethod
+    def update_activity(self, activity_id: uuid.UUID, title=None, description=None, start_date=None, end_date=None, xp_reward=None, status=None) -> Activity | None:
+        pass
+
+    @abstractmethod
+    def delete_activity(self, activity_id: uuid.UUID) -> bool:
         pass
