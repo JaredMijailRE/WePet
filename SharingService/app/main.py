76a7a4f0a324a643b7db0ae1ae606638da3cd1ab
jvaquiro <jvaquiro@unal.edu.co>
2025-12-02 18:53:09 -0500

    feat(SharingService): Integrate emotional report feature into main application

--- SharingService/app/main.py
+++ SharingService/app/main.py
@@ -1,11 +1,38 @@
 from fastapi import FastAPI
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from app.interface.http.routers import router
+from app.adapter.db.database import engine, SessionLocal
+from app.adapter.db import models
 
+
+models.Base.metadata.create_all(bind=engine)
+def seed_emotions():
+    db = SessionLocal()
+    try:
+        
+        emotions = [
+            "happy", "excited", "calm", "tired",
+            "proud", "jealous", "worried", "sad",
+            "surprised", "scared", "shy", "angry"
+        ]
+        for emotion_name in emotions:
+            exists = db.query(models.EmotionalStatus).filter_by(name=emotion_name).first()
+            if not exists:
+                db.add(models.EmotionalStatus(name=emotion_name))
+        db.commit()
+        print("Emociones inicializadas en DB")
+    except Exception as e:
+        print(f"Error seeding emotions: {e}")
+    finally:
+        db.close()
+        
+seed_emotions()
 app = FastAPI(root_path="/sharing")
 
 app.add_middleware(
     TrustedHostMiddleware, allowed_hosts=["*"]
 )
+app.include_router(router, tags=["Emotional Report"])
 
 
 
