d842fb551671b125f40f785ddfefb60e80d04d5c
jvaquiro <jvaquiro@unal.edu.co>
2025-12-02 18:53:08 -0500

    feat(SharingService): Add get_db dependency and update emotional report models

--- SharingService/app/adapter/db/models.py
+++ SharingService/app/adapter/db/models.py
@@ -1,21 +1,26 @@
 from datetime import datetime, timezone
 import uuid
 
-from sqlalchemy import Column, TIMESTAMP, Integer, ForeignKey,String, DECIMAL
+from sqlalchemy import Column, DateTime, Integer, ForeignKey,String, DECIMAL, TIMESTAMP
+
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 from .database import Base
 
-class EmotionalReport(Base):
+
+class EmotionalReportModel(Base):
     __tablename__ = "emotional_reports"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id = Column(UUID(as_uuid=True), nullable=False)
-    status_id = Column(Integer, ForeignKey("emotional_status.id"), nullable=False)
-    group_id = Column(UUID(as_uuid=True), nullable=False)
-    reported_at = Column(TIMESTAMP(timezone=True), default=datetime.now(timezone.utc))
+    group_id = Column(UUID(as_uuid=True), nullable=False, index=True)
 
-    status = relationship("EmotionalStatus", back_populates="reports", cascade="all, delete")
+    status_id = Column(Integer, ForeignKey("emotional_status.id"), nullable=False)
+    
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+
+    status_rel = relationship("EmotionalStatus", back_populates="reports")
 
     # def __repr__(self):
     #     return f"<EmotionalReport id={self.id}>"
@@ -23,10 +28,10 @@ class EmotionalReport(Base):
 class EmotionalStatus(Base):
     __tablename__ = "emotional_status"
 
-    id = Column(Integer, primary_key=True, unique=True, nullable=False)
-    status = Column(String(20), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), unique=True, nullable=False)
 
-    reports = relationship("EmotionalReport", back_populates="status")
+    reports = relationship("EmotionalReportModel", back_populates="status_rel")
 
 class Location(Base):
     __tablename__ = "locations"
